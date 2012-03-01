b7d33fa752402a4289ada6402eadccc81b812ed6
Halmen Istvan <Halmen Istvan@localhost>
2012-03-01 09:23:01 +0200

    Remove CI from version control

--- application/config/config.php
+++ application/config/config.php
@@ -11,7 +11,7 @@
 |	http://example.com/
 |
 */
-$config['base_url']	= "/projects/carbo/src/carbogrid";
+$config['base_url']	= "/";
 
 /*
 |--------------------------------------------------------------------------
